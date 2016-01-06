6abec9e6c7bb299158b1627636268208b4b7afc7
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-05 19:39:22 -0800

    Initial Quiz code.

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Hector Enrique Gomez Morales on 1/5/16.
+//  Copyright © 2016 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
