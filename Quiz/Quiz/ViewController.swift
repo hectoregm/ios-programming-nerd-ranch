18b80c279db96d7330b01c4bd4d55e0feef23b4f
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-05 20:13:51 -0800

    Full connected UI

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+    }
 }
 
