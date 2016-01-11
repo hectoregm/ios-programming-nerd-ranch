e11611fe4f28167a8dacb2db947e1d22d9ba881d
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-11 13:27:17 -0800

    Add tap gesture to dismiss keyboard.

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -11,8 +11,17 @@ import UIKit
 class ConversionViewController: UIViewController {
     
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
     
     @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
-        celsiusLabel.text = textField.text
+        if let text = textField.text where !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
     }
 }
