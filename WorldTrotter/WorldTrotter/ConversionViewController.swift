74607bcbc084f8a47179282743bed117f632495b
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-19 19:18:23 -0800

    Add localization awareness.

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -9,84 +9,88 @@
 import UIKit
 
 class ConversionViewController: UIViewController, UITextFieldDelegate {
+  
+  @IBOutlet var celsiusLabel: UILabel!
+  @IBOutlet var textField: UITextField!
+  
+  var fahrenheitValue: Double? {
+    didSet {
+      updateCelsiusLabel()
+    }
+  }
+  
+  var celsiusValue: Double? {
+    if let value = fahrenheitValue {
+      return (value - 32) * (5/9.0)
+    } else {
+      return nil
+    }
+  }
+  
+  let numberFormatter: NSNumberFormatter = {
+    let nf = NSNumberFormatter()
+    nf.numberStyle = .DecimalStyle
+    nf.minimumFractionDigits = 0
+    nf.maximumFractionDigits = 1
+    return nf
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet var celsiusLabel: UILabel!
-    @IBOutlet var textField: UITextField!
+    print("ConversionViewController loaded its view.")
+  }
+  
+  @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+    if let text = textField.text, number = numberFormatter.numberFromString(text) {
+      fahrenheitValue = number.doubleValue
+    } else {
+      fahrenheitValue = nil
+    }
+  }
+  
+  @IBAction func dismissKeyboard(sender: AnyObject) {
+    textField.resignFirstResponder()
+  }
+  
+  func updateCelsiusLabel() {
+    if let value = celsiusValue {
+      celsiusLabel.text = numberFormatter.stringFromNumber(value)
+    } else {
+      celsiusLabel.text = "???"
+    }
+  }
+  
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    let currentLocale = NSLocale.currentLocale()
+    let decimalSeparator = currentLocale.objectForKey(NSLocaleDecimalSeparator) as! String
+    let thousandsSeparator = currentLocale.objectForKey(NSLocaleGroupingSeparator) as! String
     
-    var fahrenheitValue: Double? {
-        didSet {
-            updateCelsiusLabel()
-        }
+    let existingTextHasDecimalSeparator = textField.text?.rangeOfString(decimalSeparator)
+    let replacementTextHasDecimalSeparator = string.rangeOfString(decimalSeparator)
+    
+    let doubleDecimal = existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil
+    
+    let decimalDigitSet = NSCharacterSet(charactersInString: "01234567890\(decimalSeparator)\(thousandsSeparator)")
+    
+    var invalidNumber = false
+    for character in string.characters {
+      if !charset(decimalDigitSet, containsCharacter: character) {
+        invalidNumber = true
+        break
+      }
     }
     
-    var celsiusValue: Double? {
-        if let value = fahrenheitValue {
-            return (value - 32) * (5/9.0)
-        } else {
-            return nil
-        }
+    if  doubleDecimal || invalidNumber {
+      return false
+    } else {
+      return true
     }
-    
-    let numberFormatter: NSNumberFormatter = {
-        let nf = NSNumberFormatter()
-        nf.numberStyle = .DecimalStyle
-        nf.minimumFractionDigits = 0
-        nf.maximumFractionDigits = 1
-        return nf
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("ConversionViewController loaded its view.")
-    }
-    
-    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
-        if let text = textField.text, value = Double(text) {
-            fahrenheitValue = value
-        } else {
-            fahrenheitValue = nil
-        }
-    }
-    
-    @IBAction func dismissKeyboard(sender: AnyObject) {
-        textField.resignFirstResponder()
-    }
-    
-    func updateCelsiusLabel() {
-        if let value = celsiusValue {
-            celsiusLabel.text = numberFormatter.stringFromNumber(value)
-        } else {
-            celsiusLabel.text = "???"
-        }
-    }
-    
-    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
-        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
-        
-        let doubleDecimal = existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil
-        
-        let decimalDigitSet = NSCharacterSet(charactersInString: "01234567890.")
-
-        var invalidNumber = false
-        for character in string.characters {
-            if !charset(decimalDigitSet, containsCharacter: character) {
-                invalidNumber = true
-                break
-            }
-        }
-        
-        if  doubleDecimal || invalidNumber {
-            return false
-        } else {
-            return true
-        }
-    }
-    
-    func charset(cset: NSCharacterSet, containsCharacter c: Character) -> Bool {
-        let s = String(c)
-        let result = s.rangeOfCharacterFromSet(cset)
-        return result != nil
-    }
+  }
+  
+  func charset(cset: NSCharacterSet, containsCharacter c: Character) -> Bool {
+    let s = String(c)
+    let result = s.rangeOfCharacterFromSet(cset)
+    return result != nil
+  }
 }
