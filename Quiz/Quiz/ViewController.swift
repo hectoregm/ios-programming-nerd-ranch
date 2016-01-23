e42df9572c5b8d7ae76d239ec5502ceea88f574f
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-22 20:09:11 -0800

    Add animations between questions.

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
   @IBOutlet var currentQuestionLabel: UILabel!
+  @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
   @IBOutlet var nextQuestionLabel: UILabel!
+  @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
   @IBOutlet var answerLabel: UILabel!
 
   let questions: [String] = ["From what is cognac made ?",
@@ -25,6 +27,8 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     currentQuestionLabel.text = questions[currentQuestionIndex]
+
+    updateOffScreenLabel()
   }
 
   override func viewWillAppear(animated: Bool) {
@@ -51,13 +55,31 @@ class ViewController: UIViewController {
     answerLabel.text = answer
   }
 
+  func updateOffScreenLabel() {
+    let screenWidth = view.frame.width
+    nextQuestionLabelCenterXConstraint.constant = -screenWidth
+  }
+
   func animateLabelTransitions() {
-    UIView.animateWithDuration(0.5, delay: 0, options: [], animations: {
-      self.currentQuestionLabel.alpha = 0
-      self.nextQuestionLabel.alpha = 1
+    view.layoutIfNeeded()
+
+    let screenWidth = view.frame.width
+    self.nextQuestionLabelCenterXConstraint.constant = 0
+    self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+
+    UIView.animateWithDuration(0.5, delay: 0, options: [],
+      animations: {
+        self.currentQuestionLabel.alpha = 0
+        self.nextQuestionLabel.alpha = 1
+
+        self.view.layoutIfNeeded()
       }, completion: { _ in
         swap(&self.currentQuestionLabel,
              &self.nextQuestionLabel)
+        swap(&self.currentQuestionLabelCenterXConstraint,
+             &self.nextQuestionLabelCenterXConstraint)
+
+        self.updateOffScreenLabel()
     })
   }
 }
