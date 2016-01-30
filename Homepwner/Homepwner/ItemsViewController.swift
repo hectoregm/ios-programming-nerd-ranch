cf2db546e6e5e4a8031f4ebd1f4ca5355857159e
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-30 13:12:12 -0600

    Add editing mode to tableview.

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -11,6 +11,32 @@ import UIKit
 class ItemsViewController: UITableViewController {
   var itemStore: ItemStore!
 
+  @IBAction func addNewItem(sender: AnyObject) {
+
+  }
+
+  @IBAction func toggleEditingMode(sender: AnyObject) {
+    if editing {
+      sender.setTitle("Edit", forState: .Normal)
+
+      setEditing(false, animated: true)
+    } else {
+      sender.setTitle("Done", forState: .Normal)
+
+      setEditing(true, animated: true)
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+
+    let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+    tableView.contentInset = insets
+    tableView.scrollIndicatorInsets = insets
+  }
+
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return itemStore.allItems.count
   }
